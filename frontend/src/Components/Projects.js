f5f5245023c6461937e0c1f1adf9c2f9b5401604
Abdulaziz Ali <guuree1982@gmail.com>
2022-11-19 17:13:08 -0500

    added projects section

---
+++ frontend/src/Components/Projects.js
@@ -0,0 +1,17 @@
+
+import './Project.css'
+
+
+function project(){
+    return(
+
+        <div className='project_p'>
+            <div>project1</div>
+            <div>project1</div>
+            <div>project1</div>
+
+        </div>
+    )
+}
+
+export default project;
