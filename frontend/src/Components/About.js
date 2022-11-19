1ad39eeae89f8f769b2135ecbd0f7939d2bf5eee
Abdulaziz Ali <guuree1982@gmail.com>
2022-11-19 11:27:16 -0500

    Added home ,about,contact pages and working on them

---
+++ frontend/src/Components/About.js
@@ -0,0 +1,16 @@
+import "./About.css"
+
+function About(){
+    return(
+        <div className="about_page">
+            <h1>About me</h1>
+            <p>
+            I am a professional Full Stack Developer with knowledge of various programming languages,Frameworks/Libraries like NodeJS, ReactJS, MongoDB, JavaScript, HTML, CSS etc.
+            I completed my 400 hour boatcamp in New Jersey institute of Technology in 2022. 
+            </p>
+
+        </div>
+    )
+}
+
+export default About;
