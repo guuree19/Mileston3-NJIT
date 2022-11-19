d74f91357daa8b1f0675a9caebc3681f908609bb
Abdulaziz Ali <guuree1982@gmail.com>
2022-11-19 12:23:54 -0500

    working on contact page

--- frontend/src/Components/Contact.js
+++ frontend/src/Components/Contact.js
@@ -4,11 +4,18 @@ function Contact(){
     return(
         <div className="contact_p">
             <h1>
-                
+                get in touch 
             </h1>
-
+            <form>
+                <label for='fname'/><br/>
+                First name:
+                <input type='name'/>
+                <label for='fname'/><br/>
+                Last name:
+                <input type='lame'/><br/>
+                <label for='submit' vlue='submit'/>
+            </form>
         </div>
-
     )
 }
 
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -3,7 +3,7 @@ import "./Home.css"
 function Home(){
     return(
         <div className="home_P">
-            <p>Hello, welecome <b>I'm Abdulaziz</b> just finised NJIT boatcamp.</p>
+            <p>Hello, welecome <b>I'm Abdulaziz,</b> just finised NJIT boatcamp.</p>
         </div>
 
     )
